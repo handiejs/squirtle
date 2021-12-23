9493675634df73e8d826b43e81c6518dd6ad552f
Ourai Lin <ourairyu@gmail.com>
2021-12-23 17:07:10 +0800

    fix: style of `TableView` widget

--- src/view/table-view/TableView.tsx
+++ src/view/table-view/TableView.tsx
@@ -2,9 +2,16 @@ import { ReactNode } from 'react';
 
 import { TableViewHeadlessWidget } from 'handie-react/dist/widgets';
 
+import style from './style.scss';
+
 export default class TableViewWidget extends TableViewHeadlessWidget {
+  constructor(props) {
+    super(props);
+    this.setStyleClassNames(style);
+  }
+
   public render(): ReactNode {
-    const classNames = ['TableView'];
+    const classNames = [this.getStyleClassName('TableView')];
 
     if (this.config.className) {
       classNames.push(this.config.className);
