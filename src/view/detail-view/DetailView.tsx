13ed6a2af03e1705dc33171c31d1bafcfe5c7ebb
Ourai Lin <ourairyu@gmail.com>
2022-01-05 17:39:44 +0800

    refactor: use `renderForm()` method from `FormViewStructuralWidget`

--- src/view/detail-view/DetailView.tsx
+++ src/view/detail-view/DetailView.tsx
@@ -1,39 +1,10 @@
 import { ReactNode } from 'react';
+import { FormViewStructuralWidget } from 'handie-react/dist/widgets';
 
-import { ComponentCtor, getRenderer } from 'handie-react';
-import { ObjectViewStructuralWidget } from 'handie-react/dist/widgets';
-
-export default class DetailViewWidget extends ObjectViewStructuralWidget {
-  private get id() {
-    return '';
-    // return this.$route.params.id || '';
-  }
-
-  public componentDidMount(): void {
-    const ctx = this.context;
-
-    if (this.id && ctx.getOne) {
-      ctx.getOne(this.id, (data) => {
-        this.setState({ dataSource: data });
-        this.context.setValue(data);
-      });
-    }
-  }
-
+export default class DetailViewWidget extends FormViewStructuralWidget {
   public render(): ReactNode {
-    const FormRenderer = getRenderer('FormRenderer') as ComponentCtor;
-
     return (
-      <div className="DetailView">
-        {FormRenderer ? (
-          <FormRenderer
-            fields={this.fields}
-            value={this.state.value}
-            readonly={true}
-            config={this.config}
-          />
-        ) : null}
-      </div>
+      <div className="DetailView">{this.renderForm({ readonly: true })}</div>
     );
   }
 }
--- src/view/form-view/FormView.tsx
+++ src/view/form-view/FormView.tsx
@@ -1,40 +1,8 @@
 import { ReactNode } from 'react';
+import { FormViewStructuralWidget } from 'handie-react/dist/widgets';
 
-import { ComponentCtor, getRenderer } from 'handie-react';
-import { ObjectViewStructuralWidget } from 'handie-react/dist/widgets';
-
-export default class FormViewWidget extends ObjectViewStructuralWidget {
-  private get id() {
-    return '';
-    // return this.$route.params.id || '';
-  }
-
-  public componentDidMount(): void {
-    const ctx = this.context;
-
-    if (this.id && ctx.getOne) {
-      ctx.getOne(this.id, (data) => {
-        this.setState({ dataSource: data });
-        this.context.setValue(data);
-      });
-    }
-  }
-
+export default class FormViewWidget extends FormViewStructuralWidget {
   public render(): ReactNode {
-    const FormRenderer = getRenderer('FormRenderer') as ComponentCtor;
-
-    return (
-      <div className="FormView">
-        {FormRenderer ? (
-          <FormRenderer
-            fields={this.fields}
-            value={this.state.value}
-            validation={this.state.validation}
-            config={this.config}
-            onChange={this.onFieldValueChange}
-          />
-        ) : null}
-      </div>
-    );
+    return <div className="FormView">{this.renderForm()}</div>;
   }
 }
