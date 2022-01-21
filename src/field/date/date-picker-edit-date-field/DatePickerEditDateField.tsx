cc771866010d0ab5cc122c3a79e93d353b6634b5
Ourai Lin <ourairyu@gmail.com>
2022-01-21 22:31:22 +0800

    feat: date picker and date range picker widgets for date field

---
+++ src/field/date/date-picker-edit-date-field/DatePickerEditDateField.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+
+import { ComponentCtor, DateValue, getControl, pick } from 'handie-react';
+import { DateFieldStructuralWidget } from 'handie-react/dist//widgets/class';
+
+export default class DatePickerEditDateFieldWidget extends DateFieldStructuralWidget<DateValue> {
+  public render(): ReactNode {
+    const DatePicker = getControl('DatePicker') as ComponentCtor;
+
+    return DatePicker ? (
+      <DatePicker
+        value={this.props.value}
+        placeholder={this.getPlaceholder()}
+        disabled={this.state.disabled}
+        format={this.config.format}
+        pickerOption={pick(this.config, ['disableDate', 'showToday'])}
+        onChange={(_, date) => this.onDateChange(date)}
+      />
+    ) : null;
+  }
+}
---
+++ src/field/date/index.ts
@@ -0,0 +1,4 @@
+export { default as DatePickerEditDateFieldWidget } from './date-picker-edit-date-field';
+export { default as DatePickerReadDateFieldWidget } from './date-picker-read-date-field';
+export { default as DateRangePickerEditDateFieldWidget } from './date-range-picker-edit-date-field';
+export { default as DateRangePickerReadDateFieldWidget } from './date-range-picker-read-date-field';
