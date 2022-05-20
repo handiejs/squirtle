b230873971fad241d17bf606c7ba11fd13d2f164
Ourai Lin <ourairyu@gmail.com>
2022-05-20 17:16:55 +0800

    chore: remove `DialogViewButtonActionWidget`

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,5 +1,3 @@
 export { default as ButtonActionWidget } from './button-action';
 export { default as LinkActionWidget } from './link-action';
 export { default as IconActionWidget } from './icon-action';
-
-export { default as DialogViewButtonActionWidget } from './dialog-view-button-action';
